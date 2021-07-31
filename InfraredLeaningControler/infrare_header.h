0aef2cb96150a027c1a3d35dfee9eaa7287dcd43
raqqona <e1n18047@st.oit.ac.jp>
2021-07-31 21:46:48 +0900

    add global item valu

--- InfraredLeaningControler/infrare_header.h
+++ InfraredLeaningControler/infrare_header.h
@@ -1,13 +1,14 @@
 #ifndef INCLUDE_INFRARE
 #define INCLUDE_INFRARE
 
-extern uint16_t IR_send_pin = 16;
+extern uint16_t IR_send_pin = 17;
 extern decode_type_t maker_code = "MITSUBISHI112";
 extern uint16_t IR_bits = 112;
+extern uint16_t command_header = 0x23CB2601002;
 
 
 void InfrareSendInit();
 void InfrareSend();
-unsigned char MakeCommand();
+unsigned char MakeCommand(char *power, char *mode, char *temp, char *fan, char *swing);
 
 #endif
