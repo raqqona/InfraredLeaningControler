494e3eacfb784bcc84f5f1d4eca9b2edee76d6c7
raqqona <e1n18047@st.oit.ac.jp>
2021-08-02 20:14:40 +0900

    make func of generating data to be sent from the remote control

--- InfraredLeaningControler/infrare.cpp
+++ InfraredLeaningControler/infrare.cpp
@@ -12,11 +12,75 @@ void InfrareSendInit(){
 }
 
 void InfrareSend(){
+    unsigned char command[28];
     command = MakeCommand();
     irsend.send(maker_code, command, IR_bits);
 }
 
-unsigned char MakeCommand() {
-    unsigned char command[500];
-    //next task
+unsigned char *MakeCommand(char *power, char *mode, int temp, char *fan, char *swing) {
+    unsigned char command[28];
+    command = COMMNAD_HEADER;
+    if (power = "ON"){
+        command[11] = POWER_ON;
+
+    }else if(power = "OFF"){
+        command[11] = POWER_OFF;
+    }else {
+        return "failed";
+    }
+    command[12] = 0x0
+    if (mode = "COOL"){
+        command[13] = MODE_COOL;
+    }else if (mode = "DRY") {
+        command[13] = MODE_DRY;
+    }else if (mode = "HEAT") {
+        command[13] = MODE_HEAT;
+    }else {
+        return "failed";
+    }
+    if (temp > 31 || temp < 18) {
+        return "failed";
+    }
+    command[14] = 0x0;
+    command[15] = (unsigned char)(31 - temp);
+    if (swing == "UNKNOWN") {
+        command[16] = SWING_UNKNOWN;
+    }else if (swing == "HIGHEST") {
+        command[16] =SWING_HIGHEST;
+    }else if (swing == "HIGH") {
+        command[16] =SWING_HIGH;
+    }else if (swing == "MIDDLE") {
+        command[16] =SWING_MIDDLE;
+    }else if (swing == "LOW") {
+        command[16] =SWING_LOW;
+    }else if (swing == "LOWEST") {
+        command[16] =SWING_LOWEST;
+    }else if (swing == "AUTO") {
+        command[16] =SWING_AUTO;
+    }else {
+        return "failed";
+    }
+    if (fan == "HIGH" && swing == "UNKNOWN" || swing == "HIGH" || swing == "LOW" || swing == "LOWEST") {
+        command[17] = FAN_HIGH << 8;
+    }else if (fan == "HIGH" && swing == "HIGHEST" || swing == "MIDDLE" || swing == "AUTO") {
+        command[17] = FAN_HIGH;
+    }else if (fan == "MIDIUM" && swing == "UNKNOWN" || swing == "HIGH" || swing == "LOW" || swing == "LOWEST") {
+        command[17] = FAN_MIDIUM << 8;
+    }else if (fan == "MIDIUM" && swing == "HIGHEST" || swing == "MIDDLE" || swing == "AUTO") {
+        command[17] = FAN_MIDIUM;
+    }else if (fan == "LOW" && swing == "UNKNOWN" || swing == "HIGH" || swing == "LOW" || swing == "LOWEST") {
+        command[17] == FAN_LOW << 8;
+    }else if (fan == "LOW" && swing == "HIGHEST" || swing == "MIDDLE" || swing == "AUTO") {
+        command[17] == FAN_LOW;
+    }else if (fan == "QUIET" && swing == "UNKNOWN" || swing == "HIGH" || swing == "LOW" || swing == "LOWEST") {
+        command[17] == FAN_QUIET << 8;
+    }else if (fan == "QUIET" && swing == "HIGHEST" || swing == "MIDDLE" || swing == "AUTO") {
+        command[17] == FAN_QUIET;
+    }else {
+        return "failed";
+    }
+    command[18] = 0x00000000
+    command[26] = PARITY + command[13] + command[15] + (command[16] << 1) + command[17];
+
+    return commnad;
 }
--- InfraredLeaningControler/infrare_header.h
+++ InfraredLeaningControler/infrare_header.h
@@ -2,31 +2,31 @@
 #define INCLUDE_INFRARE
 
 extern uint16_t IR_send_pin = 17;
-extern decode_type_t maker_code = "MITSUBISHI112";
 extern uint16_t IR_bits = 112;
-extern uint16_t command_header = 0x23CB2601002;
+extern uint16_t COMMAND_HEADER = 0x23CB2601002;
 
-extern uint8_t POWER_ON = 4
-extern uint8_t POWER_OFF = 0
-extern uint8_t MODE_HEAT = 1
-extern uint8_t MODE_DRY = 4
-extern uint8_t MODE_COOL = 3
-extern uint8_t FAN_HIGH = 0
-extern uint8_t FAN_MEDIUM = 5
-extern uint8_t FAN_LOW = 3
-extern uint8_t FAN_QUIET = 2
-extern uint8_t SWING_UNKNOWN = 0
-extern uint8_t SWING_HIGHEST = 1
-extern uint8_t SWING_HIGH = 2
-extern uint8_t SWING_MIDDLE = 3
-extern uint8_t SWING_LOW = 4
-extern uint8_t SWING_LOWEST = 5
-extern uint8_t SWING_AUTO = 7
+extern uint8_t POWER_ON = 0x4
+extern uint8_t POWER_OFF = 0x0
+extern uint8_t MODE_HEAT = 0x1
+extern uint8_t MODE_DRY = 0x4
+extern uint8_t MODE_COOL = 0x3
+extern uint8_t FAN_HIGH = 0x0
+extern uint8_t FAN_MEDIUM = 0x5
+extern uint8_t FAN_LOW = 0x3
+extern uint8_t FAN_QUIET = 0x2
+extern uint8_t SWING_UNKNOWN = 0x0
+extern uint8_t SWING_HIGHEST = 0x1
+extern uint8_t SWING_HIGH = 0x1
+extern uint8_t SWING_MIDDLE = 0x2
+extern uint8_t SWING_LOW = 0x2
+extern uint8_t SWING_LOWEST = 0x3
+extern uint8_t SWING_AUTO = 0x0
+extern uint8_t PARITY = 0x30
 
 
 
 void InfrareSendInit();
 void InfrareSend();
-unsigned char MakeCommand(char *power, char *mode, char *temp, char *fan, char *swing);
+unsigned char *MakeCommand(char *power, char *mode, char *temp, char *fan, char *swing);
 
 #endif
